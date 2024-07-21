c25f740bec8cea86450292c0b086e7e570a34b6d
jotyot <104177975+jotyot@users.noreply.github.com>
2024-07-21 14:21:06 -0700

    jisho api is prolly good enough

--- jishoAPI/fetchVocab.js
+++ jishoAPI/fetchVocab.js
@@ -1,6 +1,20 @@
 require('dotenv').config()
 const wanikaniAPIkey = process.env.WANIKANI_API_KEY
 
+/*
+    The VocabInfo object has the following properties:
+    word: string
+    reading: string
+    meaning: string[]
+    partsOfSpeech: string[]
+    jlptLevel: string
+    sentences: {japanese: string, english: string}[]
+*/
+
+/*
+    If word is found, returns a VocabInfo object
+    Else returns null
+*/
 const getWanikaniVocab = async (word) => {
     const response = await fetch(`https://api.wanikani.com/v2/subjects?types=vocabulary&slugs=${word}`, {
         headers: {
@@ -38,17 +52,37 @@ const getJishoVocab = async (word) => {
         meaning: firstResult.senses[0].english_definitions,
         partsOfSpeech: firstResult.senses[0].parts_of_speech,
         jlptLevel: firstResult.jlpt[0],
+        sentences: []
     }
-    const sentences = await jisho.searchForExamples(word)
-    // Needs a more sophisticated way to choose sentences
-    response.sentences = sentences.results.slice(0, 3).map(sentence => ({
-        japanese: sentence.kanji,
-        english: sentence.english,
-    }))
     return response
 }
 
+const getJishoSentences = async (word) => {
+    const sentences = await jisho.searchForExamples(word)
+    // Needs a more sophisticated way to choose sentences
+    return sentences.results.slice(0, 3).map(sentence => ({
+        japanese: sentence.kanji,
+        english: sentence.english,
+    }))
+}
+
+/*
+    Prefers Wanikani data, gets the jlpt level from Jisho
+    If not found in Wanikani, gets everything from Jisho
+    Returns a VocabInfo object
+*/
+const createVocabInfo = async (word) => {
+    const wanikaniResult = await getWanikaniVocab(word)
+    const jishoResult = await getJishoVocab(word)
+    if (wanikaniResult) {
+        wanikaniResult.jlptLevel = jishoResult.jlptLevel
+        return wanikaniResult
+    } else {
+        jishoResult.sentences = await getJishoSentences(word)
+        return jishoResult
+    }
+}
+
 module.exports = {
-    getWanikaniVocab,
-    getJishoVocab
+    createVocabInfo
 }
--- jishoAPI/index.js
+++ jishoAPI/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const { getJishoVocab, getWanikaniVocab } = require('./fetchVocab.js')
+const { createVocabInfo } = require('./fetchVocab.js')
 
 app.get('/lookup/:word', async (req, res) => {
     
     const word = req.params.word
     try {
-        const result = await getWanikaniVocab(word) ?? await getJishoVocab(word)
+        const result = await createVocabInfo(word)
         res.send(result)
     } catch (error) {
         res.status(500).send('An error occurred')
