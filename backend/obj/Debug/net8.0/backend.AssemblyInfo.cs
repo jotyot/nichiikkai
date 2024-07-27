8461cd0bed311fc455c88169dcb305da6ddfa2c7
jotyot <104177975+jotyot@users.noreply.github.com>
2024-07-26 21:45:30 -0700

    I MEAN SURELY

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e5b61ad237a60e742c7cfc10f285b4b3b018e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a4125b3b4941e3b8d53ea5e080cd2ef7eedaa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
