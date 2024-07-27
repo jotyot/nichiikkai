2956d1371269a259b6098896fdc6523643eaf7ad
jotyot <104177975+jotyot@users.noreply.github.com>
2024-07-26 21:51:13 -0700

    waaa

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f3b6306b67f9cddbf47560849154915dd14cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc31026f106b748036bf6a6781f0d9518a6c9a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
