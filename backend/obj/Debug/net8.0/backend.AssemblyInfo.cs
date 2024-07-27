179820d9c7f2f08083cd4233566b78b054c0cf62
jotyot <104177975+jotyot@users.noreply.github.com>
2024-07-26 21:40:16 -0700

    ignore fr

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04cbd88b2f73ab526cac64d5c7c0c1c4e66f818")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbaa975581a03be0d2bb637c69aed0ad64b278b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
