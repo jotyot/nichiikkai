83de3f9f956ed45433dddf2bee940ce60c51f29d
jotyot <104177975+jotyot@users.noreply.github.com>
2024-08-08 22:11:25 -0700

    removed like A HUNDRED WORDS WHAT

--- jishoAPI/fetchSentences.js
+++ jishoAPI/fetchSentences.js
@@ -1,50 +1,3 @@
-// For okurigana words, truncate the word and reading to match, get the first one for kanji-kana-kanji-kana words i cant be bothered
-const truncateMatchingCharacters = (word, reading) => {
-  for (let i = 0; i < word.length; i++) {
-    for (let j = i; j < reading.length; j++) {
-      if (word[i] === reading[j]) {
-        return [word.slice(0, i), reading.slice(0, j)];
-      }
-    }
-  }
-  return [word, reading];
-};
-
-/*
-    Use sentence transcriptions to filter out sentences that don't match the reading
-  */
-const matchReadingSentences = (sentences, word, reading) => {
-  // filter out sentences that don't contain the word
-  sentences = sentences.filter((sentence) => sentence.text.includes(word));
-
-  if (reading === undefined) return sentences;
-  if (reading === word) return sentences;
-
-  [word, reading] = truncateMatchingCharacters(word, reading);
-
-  return sentences.filter((sentence) => {
-    const transcription = sentence.transcriptions[0].text;
-    const regex = /\[(.*?)\]/g;
-    const matches = transcription.match(regex);
-    if (matches == null) return false;
-
-    // [[kanji, kana, kana, ...], [kanji, kana, ...], ...]
-    // assuming the format is whole kanji word, with kana readings of each kanji
-    const words = matches.map((match) => match.slice(1, -1).split("|"));
-    return words.some((wordArray) => {
-      const wordStartIndex = wordArray[0].indexOf(word[0]);
-      if (wordStartIndex === -1) return false;
-
-      // try to match the kanjis in wordArray[0] to the kanas in wordArray
-      return (
-        wordArray
-          .slice(wordStartIndex + 1, word.length + wordStartIndex + 1)
-          .join("") === reading
-      );
-    });
-  });
-};
-
 const isVerb = (jishoResult) => {
   const pos = jishoResult.partsOfSpeech.map((entry) => entry.toLowerCase());
   if (pos.includes("suru verb")) return false;
@@ -68,7 +21,7 @@ const getTatoebaSentences = async (jishoResult, pageNo = 1) => {
   console.log(`Getting sentences for ${word} (${reading}) on page ${pageNo}`);
 
   const result = await fetch(
-    `https://tatoeba.org/en/api_v0/search?from=jpn&query=${word}&to=eng&page=${pageNo}`
+    `https://tatoeba.org/en/api_v0/search?from=jpn&query=@text+"${word}"+@transcription+${reading}&to=eng&page=${pageNo}`
   );
 
   if (!result.ok) return [];
@@ -78,7 +31,7 @@ const getTatoebaSentences = async (jishoResult, pageNo = 1) => {
 
   if (data.length === 0) return [];
 
-  const sentences = matchReadingSentences(data, word, reading)
+  const sentences = data
     .slice(0, numSentences)
     .map((sentence) => {
       try {
