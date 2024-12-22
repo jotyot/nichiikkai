f2199f6abae8891ff6a3ee099ef5637527852503
jotyot <104177975+jotyot@users.noreply.github.com>
2024-12-22 15:58:29 -0800

    ok i could prolly try exporting this

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,9 +3,12 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { StyleSheet } from "react-native";
+import ThemedView from "@/components/themed/ThemedView";
+import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,18 +31,40 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="register" options={{ headerShown: false }} />
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="signing-in" options={{ headerShown: false }} />
-        <Stack.Screen name="fetching-data" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="reviews" options={{ headerShown: false }} />
-        <Stack.Screen name="learn" options={{ headerShown: false }} />
-        <Stack.Screen name="select-levels" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <ThemedView style={styles.container}>
+        <SafeAreaView style={styles.appContainer}>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="register" options={{ headerShown: false }} />
+            <Stack.Screen name="login" options={{ headerShown: false }} />
+            <Stack.Screen name="signing-in" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="fetching-data"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="reviews" options={{ headerShown: false }} />
+            <Stack.Screen name="learn" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="select-levels"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </SafeAreaView>
+      </ThemedView>
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  appContainer: {
+    flex: 1,
+    maxWidth: 500,
+    width: "100%",
+  },
+});
